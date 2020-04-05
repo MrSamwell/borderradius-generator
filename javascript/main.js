b13cdba86c097d40eeb02c3ddfdd09db9ca38ebe
MrSamwell <samuelbs98@gmail.com>
2020-04-05 05:27:27 -0300

    Funcional

---
+++ javascript/main.js
@@ -0,0 +1,51 @@
+new Vue({
+    el:'#app',
+    data:
+    {
+        cornertl: 0,
+        cornertr: 0,
+        cornerbr: 0,
+        cornerbl: 0,
+        allcorners: 0,
+        csscode: ''
+    },
+    methods:{
+        toClipBoard: function() {
+
+            const el = document.createElement('textarea');
+            el.value = this.csscode;
+            document.body.appendChild(el);
+            el.select();
+            document.execCommand('copy');
+            document.body.removeChild(el);
+            
+  
+        },
+        updateCode: function () {
+            this.csscode = 'border-radius: ' + this.cornertl + "px " + this.cornertr + "px "+ this.cornerbl + "px "+ this.cornerbr + "px;";
+            
+            let rectangle = this.$refs.rectangle;
+            rectangle.style.borderRadius =  this.cornertl + "px " + this.cornertr + "px "+ this.cornerbl + "px "+ this.cornerbr + "px";
+          },
+        allCorners: function() {
+            
+            this.cornertl = this.allcorners;
+            this.cornertr = this.allcorners;
+            this.cornerbr = this.allcorners;
+            this.cornerbl = this.allcorners;
+            this.updateCode();
+        },
+        btnReset: function() {
+            
+            this.cornertl = 0;
+            this.cornertr = 0;
+            this.cornerbr = 0;
+            this.cornerbl = 0;
+            this.allcorners=0;
+            this.updateCode();
+        }
+    },
+    mounted: function(){
+        this.updateCode();
+    }
+})
